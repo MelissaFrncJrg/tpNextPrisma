97d7e03c5c824a26d1ac90620c99f8a5aa53c3df
Melissa França Jorge <melissafranca3@gmail.com>
2025-02-04 11:05:04 +0100

    initial commit

---
+++ src/app/api/toto/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { request } from "http";
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  const users = await prisma.user.findMany({});
+  return NextResponse.json({ message: "Hello World", users: users });
+};
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+  const newUser = await prisma.user.create({
+    data: {
+      email: body.data.name + "@gmail.com",
+      name: body.data.name,
+    },
+  });
+  return NextResponse.json({ message: `Hi, ${body.data.name}`, user: newUser });
+};
