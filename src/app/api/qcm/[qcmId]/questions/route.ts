171d39507e752c09b0b15cac5fc177e329d09ccf
Melissa França Jorge <melissafranca3@gmail.com>
2025-02-04 15:48:05 +0100

    dynamic qcmIds

--- src/app/api/qcm/[qcmId]/questions/route.ts
+++ src/app/api/qcm/[qcmId]/questions/route.ts
@@ -3,7 +3,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const POST = async (req: NextRequest) => {
+export const POST = async (
+  req: NextRequest,
+  { params }: { params: { qcmId: string } }
+) => {
+  const qcmId = parseInt(params.qcmId, 10);
+
+  if (isNaN(qcmId)) {
+    return NextResponse.json({ error: "Invalid QCM ID" }, { status: 400 });
+  }
+
   const { data } = await req.json();
   const {
     content,
@@ -11,10 +20,18 @@ export const POST = async (req: NextRequest) => {
   }: { content: string; choices: { content: string; isCorrect: boolean }[] } =
     data;
 
+  const qcm = await prisma.qCM.findUnique({
+    where: { id: qcmId },
+  });
+
+  if (!qcm) {
+    return NextResponse.json({ error: "QCM not found" }, { status: 404 });
+  }
+
   const newQuestion = await prisma.question.create({
     data: {
       content,
-      qcmId: 1,
+      qcmId,
       choices: {
         create: choices.map((choice) => ({
           content: choice.content,
@@ -22,6 +39,7 @@ export const POST = async (req: NextRequest) => {
         })),
       },
     },
+    include: { choices: true },
   });
 
   return NextResponse.json(newQuestion, { status: 201 });
--- src/app/api/qcm/route.ts
+++ src/app/api/qcm/route.ts
@@ -3,6 +3,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+export const GET = async () => {
+  const qcm = await prisma.qCM.findMany({});
+  return NextResponse.json({ qcm });
+};
+
 export const POST = async (req: NextRequest) => {
   const { data } = await req.json();
   const { titre, imageSrc }: { titre: string; imageSrc?: string } = data;
